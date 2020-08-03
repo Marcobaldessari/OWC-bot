55f393c3d9feb134bcf6b2e20b47c6c92d0d6dd2
Marco Baldessari <marco.baldes@gmail.com>
2020-08-03 15:57:18 +0300

    formatting

--- commands/character.js
+++ commands/character.js
@@ -9,10 +9,10 @@ module.exports = {
 		message.channel.send(`
 Hey ${message.author.username}, let me help you with rolling stats for a new character.
 ------------
-MIG: ${char.stats.might.modifier}   (${char.stats.might.rolls})
-NIM: ${char.stats.nimb.modifier}   (${char.stats.nimb.rolls})
-DIS: ${char.stats.discipline.modifier}   (${char.stats.discipline.rolls})
-WIT: ${char.stats.wits.modifier}   (${char.stats.wits.rolls})
+MIG: ${char.stats.might.modifier}   *(${char.stats.might.rolls})*
+NIM: ${char.stats.nimb.modifier}   *(${char.stats.nimb.rolls})*
+DIS: ${char.stats.discipline.modifier}   *(${char.stats.discipline.rolls})*
+WIT: ${char.stats.wits.modifier}   *(${char.stats.wits.rolls})*
 HP: ${char.hp}
 ------------
 		`);
