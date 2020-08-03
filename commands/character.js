64ebf91cacdc5b108d9b8c057a3c32c1612b632a
Marco Baldessari <marco.baldes@gmail.com>
2020-08-03 15:56:18 +0300

    indentation

--- commands/character.js
+++ commands/character.js
@@ -7,14 +7,14 @@ module.exports = {
 	execute(message, args) {
 		var char = new Character('player');
 		message.channel.send(`
-			Hey ${message.author.username}, let me help you with rolling stats for a new character.
-			------------
-			MIG: ${char.stats.might.modifier}   (${char.stats.might.rolls})
-			NIM: ${char.stats.nimb.modifier}   (${char.stats.nimb.rolls})
-			DIS: ${char.stats.discipline.modifier}   (${char.stats.discipline.rolls})
-			WIT: ${char.stats.wits.modifier}   (${char.stats.wits.rolls})
-			HP: ${char.hp}
-			------------
+Hey ${message.author.username}, let me help you with rolling stats for a new character.
+------------
+MIG: ${char.stats.might.modifier}   (${char.stats.might.rolls})
+NIM: ${char.stats.nimb.modifier}   (${char.stats.nimb.rolls})
+DIS: ${char.stats.discipline.modifier}   (${char.stats.discipline.rolls})
+WIT: ${char.stats.wits.modifier}   (${char.stats.wits.rolls})
+HP: ${char.hp}
+------------
 		`);
 	},
 };
