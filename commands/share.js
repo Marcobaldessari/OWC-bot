6a9be0d07c7c624779cb604fe696d8d440d0b2b2
Marco Baldessari <marco.baldes@gmail.com>
2020-08-24 18:50:15 +0200

    rename

--- commands/share.js
+++ commands/share.js
@@ -1,5 +1,5 @@
 module.exports = {
-	name: 'Share bot',
+	name: 'share',
 	description: 'Install the bot on other servers',
 	execute(message, args) {
 		message.channel.send("https://discord.com/api/oauth2/authorize?client_id=734332600801558548&permissions=0&scope=bot")
