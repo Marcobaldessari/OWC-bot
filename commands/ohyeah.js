f235d279818ec39d5343dd5c4dbd9b4b57336942
Marco Baldessari <marco.baldes@gmail.com>
2020-08-25 19:01:48 +0200

    typo

--- commands/ohyeah.js
+++ commands/ohyeah.js
@@ -1,6 +1,6 @@
 module.exports = {
 	name: 'ohyeah',
-	description: 'Introduce yourself',
+	description: 'Introduce bot',
 	execute(message, args) {
         message.channel.send(`Hey I'm OWC bot and maybe I can help you with that! 
         If you head to #barrowmaze-dice-rolling and type /character I can roll attributes for you 🎲`)
