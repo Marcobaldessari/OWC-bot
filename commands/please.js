d4ae5638bc2d4ec856c14147d0ca166f01d68520
Marco Baldessari <marco.baldes@gmail.com>
2020-09-12 14:19:20 +0200

    added please build me

---
+++ commands/please.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'ohyeah',
+	description: 'Introduce bot',
+	execute(message, args) {
+		message.channel.send(`bleep blop`)
+		message.channel.send(`please help me becoming a real bot`)
+		message.channel.send(`https://giphy.com/gifs/robot-garbage-truck-I7kkegrRyNrk4`)
+	},
+};
