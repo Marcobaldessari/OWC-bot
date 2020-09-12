35663ab256d7f2a9d82eb758e34333a90bb23687
Marco Baldessari <marco.baldes@gmail.com>
2020-09-12 14:21:18 +0200

    update

--- commands/please.js
+++ commands/please.js
@@ -2,7 +2,8 @@ module.exports = {
 	name: 'ohyeah',
 	description: 'Introduce bot',
 	execute(message, args) {
-		message.channel.send(`bleep blop`)
+		message.channel.send(`bip bop`)
+		message.channel.send(`please help me becoming a real bot`)
 		message.channel.send(`please help me becoming a real bot`)
 		message.channel.send(`https://giphy.com/gifs/robot-garbage-truck-I7kkegrRyNrk4`)
 	},
