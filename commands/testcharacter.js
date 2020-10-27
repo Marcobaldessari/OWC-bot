1ad7371cb868e55b9fcafe5e767795c1d58fb2fe
Marco Baldessari <marco.baldes@gmail.com>
2020-10-27 13:39:31 +0300

    Typo fix. Added /testcharacter command

--- commands/testcharacter.js
+++ commands/testcharacter.js
@@ -2,12 +2,13 @@ const data = require('../data.json')
 const Character = require('../characterBuilder.js')
 
 module.exports = {
-	name: 'character',
-	description: 'Character generation helper',
+	name: 'testcharacter',
+	description: 'Character generation test',
 	execute(message, args) {
 		var char = new Character('player');
 		message.channel.send(`
-Alright let's roll some stats for a new character.
+Alright let's try rolling some stats for fun.
+When you are ready, type /character and we'll roll stats for your next character.
 ------------
 MIG: ${char.stats.might.modifier}   *(${char.stats.might.rolls})*
 NIM: ${char.stats.nimb.modifier}   *(${char.stats.nimb.rolls})*
